c02bf84970ae29bbe2bd53abd4817d53d66e948a
kwonih1020 <kwonih1020@gmail.com>
2022-08-08 15:59:31 +0900

    comment get 요청 기능 추가

--- src/redux/modules/commentListSlice.js
+++ src/redux/modules/commentListSlice.js
@@ -3,17 +3,51 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+// ---- 권익현 ----
+
 const initialState = {
   comments: [],
   isLoading: false,
   err: null,
 };
 
+export const __getComments = createAsyncThunk(
+  "comments/__getComments",
+  async (payload, thunkAPI) => {
+    try {
+      const targetId = payload;
+      const commentList = await axios.get(
+        `http://localhost:3001/comments?origin_id=${targetId}`
+      );
+      console.log(commentList);
+      return thunkAPI.fulfillWithValue(commentList.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.status);
+    }
+  }
+);
+
 const commentListSlice = createSlice({
   name: "commentListSlice",
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    // Comment List Reducer
+    [__getComments.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [__getComments.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.err = null;
+      state.comments = action.payload;
+    },
+    [__getComments.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.comments = [];
+      state.err = action.payload;
+    },
+  },
 });
 
 export default commentListSlice.reducer;
+// ---- 권익현 ----
