47e18fa8b9e0a02e12f6701b5d54c98b5c787fbb
kwonih1020 <kwonih1020@gmail.com>
2022-08-08 22:13:48 +0900

    post , 추가기능 추가, useParam 대신 숫자 집어넣음

--- src/components/comments/CommentForm.jsx
+++ src/components/comments/CommentForm.jsx
@@ -1,13 +1,54 @@
 // eslint-disable-next-line
 
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { useParams } from "react-router-dom";
+import { __postComments } from "../../redux/modules/commentListSlice";
 
 const CommentForm = () => {
+  const dispatch = useDispatch();
+  // const { targetId } = useParams();
+  const [inputs, setInputs] = useState({
+    user: "",
+    desc: "",
+  });
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      __postComments({
+        targetId: parseInt(1),
+        ...inputs,
+      })
+    );
+  };
+
   return (
     <>
-      <form>
-        <input type="text" placeholder="작성자" />
-        <input type="text" placeholder="댓글" />
+      <form onSubmit={onSubmitHandler}>
+        <input
+          type="text"
+          placeholder="작성자"
+          max="5"
+          name="user"
+          value={inputs.user}
+          onChange={onChangeHandler}
+        />
+        <input
+          type="text"
+          placeholder="댓글"
+          max="100"
+          name="desc"
+          value={inputs.desc}
+          onChange={onChangeHandler}
+        />
         <button>추가하기</button>
       </form>
     </>
--- src/redux/modules/commentListSlice.js
+++ src/redux/modules/commentListSlice.js
@@ -4,9 +4,16 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // ---- 권익현 ----
-
 const initialState = {
   comments: [],
+  comment: [
+    {
+      origin_id: 0,
+      id: 0,
+      user: "",
+      desc: "",
+    },
+  ],
   isLoading: false,
   err: null,
 };
@@ -27,12 +34,32 @@ export const __getComments = createAsyncThunk(
   }
 );
 
+export const __postComments = createAsyncThunk(
+  "comments/__postComments",
+  async (payload, thunkAPI) => {
+    try {
+      const commentList = await axios.get("http://localhost:3001/comments");
+      const { user, desc, targetId } = { ...payload };
+      const commentPost = await axios.post("http://localhost:3001/comments", {
+        origin_id: targetId,
+        id: commentList.data.at(-1).id + 1,
+        user,
+        desc,
+      });
+      console.log(commentPost);
+      return thunkAPI.fulfillWithValue(commentPost.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 const commentListSlice = createSlice({
   name: "commentListSlice",
   initialState,
   reducers: {},
   extraReducers: {
-    // Comment List Reducer
+    // Comment List Reducer GET
     [__getComments.pending]: (state, action) => {
       state.isLoading = true;
     },
@@ -45,6 +72,18 @@ const commentListSlice = createSlice({
       state.isLoading = false;
       state.comments = [];
       state.err = action.payload;
+    },
+    // Comment List Reducer POST
+    [__postComments.pending]: (state) => {
+      state.isLoading = true; // 네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
+    },
+    [__postComments.fulfilled]: (state, action) => {
+      state.isLoading = false; // 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.comments.push(action.payload); // Store에 있는 todos에 서버에서 가져온 todos를 넣습니다.
+    },
+    [__postComments.rejected]: (state, action) => {
+      state.isLoading = false; // 에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.error = action.payload; // catch 된 error 객체를 state.error에 넣습니다.
     },
   },
 });
